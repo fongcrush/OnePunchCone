6ea757546fc5009a4784399a062ad17e8a274851
HoYangE <syhwms@naver.com>
2021-10-14 15:56:03 +0900

    Change Enviroment prefabs

--- DeathHand2D/Assets/Scripts/Environment/PowderKeg.cs
+++ DeathHand2D/Assets/Scripts/Environment/PowderKeg.cs
@@ -7,6 +7,7 @@ public class PowderKeg : IEnvironment
 {
     public Sprite[] Sprites;
     public GameObject explosion;
+    public GameObject explosionFX;
     float Hp;
 
     private void Start()
@@ -24,6 +25,21 @@ public class PowderKeg : IEnvironment
 
     }
 
+    public void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag =="Untagged" && collision.gameObject.layer == 6 && GetComponent<BoxCollider2D>().enabled == false)
+        {
+            GM.pcStat.ChangeHP(-50);
+        }
+        if (collision.gameObject.tag == "Enemy" && GetComponent<BoxCollider2D>().enabled == false)
+        {
+            if(collision.GetComponent<EnemyController>().attTypeValue == 2 || collision.GetComponent<EnemyController>().attTypeValue == 4)
+                collision.gameObject.GetComponent<EnemyController>().Hit(500);
+            else
+                collision.gameObject.GetComponent<EnemyController>().Hit(50);
+        }
+    }
+
     public void Hit(float damage)
     {
         Hp -= damage;
@@ -35,16 +51,24 @@ public class PowderKeg : IEnvironment
 
     void StartExplosion()
     {
-        explosion.SetActive(true);
         StartEffect();
+        StartCoroutine(Explosion());
 
         GetComponent<SpriteRenderer>().sprite = Sprites[1];
-        GetComponent<BoxCollider2D>().enabled = false;
-        explosion.GetComponent<BoxCollider2D>().enabled = false;
     }
 
     void StartEffect()
     {
+        explosionFX.SetActive(true);
+        explosionFX.GetComponent<ParticleSystem>().Play();
         //불꽃 파티클 재생
+        Destroy(explosionFX, 2f);
+    }
+    IEnumerator Explosion()
+    {
+        GetComponent<BoxCollider2D>().enabled = false;
+        explosion.SetActive(true);
+        yield return null;
+        explosion.GetComponent<BoxCollider2D>().enabled = false;
     }
 }
